943ef56263e8a146ccc558ecc6600a1185dac699
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2023-05-01 00:34:02 +0000

    About component

--- ng_alban-portfolio/src/app/pages/pages.module.ts
+++ ng_alban-portfolio/src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '../shared/shared.module';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { PortfolioComponent } from './views/portfolio/portfolio.component';
 import { EducationExperienceComponent } from './views/education-experience/education-experience.component';
+import { AboutComponent } from './views/about/about.component';
 
 
 
@@ -14,7 +15,8 @@ import { EducationExperienceComponent } from './views/education-experience/educa
     AdminComponent,
     ViewsComponent,
     PortfolioComponent,
-    EducationExperienceComponent
+    EducationExperienceComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
