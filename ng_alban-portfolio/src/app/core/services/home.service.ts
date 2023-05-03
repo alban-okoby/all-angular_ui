c6d7f88d292ee0dc3994e16db300bbe8804c98fb
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2023-05-03 13:34:22 +0000

    Environment added

--- ng_alban-portfolio/src/app/core/services/home.service.ts
+++ ng_alban-portfolio/src/app/core/services/home.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HtppClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  private apiServeUrl = environment.apiBaseUrl;
+
+  constructor(private http: HtppClient) { }
 }
